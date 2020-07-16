667009e745a4141589c2f9e92eb948c6959f37dd
Zach Yingling <zpyingling9559@gmail.com>
2020-07-16 17:18:38 -0400

    getting the pop function to work in the singly linked list

--- data-struc/linkedlists/singly-linkedlists.js
+++ data-struc/linkedlists/singly-linkedlists.js
@@ -21,15 +21,51 @@ class SinglyLinkedList{
       this.head = newNode;
       this.tail = this.head;
     } else {
+      // if there is a head just make the next node after the tail the new tail
       this.tail.next = newNode;
       this.tail = newNode;
     }
+    // increment length
     this.length++;
+    // return the singlylinkedlist \/
     return this;
+  }
+  pop(){
+    // if the linked list is empty return undefined
+    if(!this.head) return undefined;
+    // if the list only has one node in the list; return the node being popped and set everything to falsey/empty list
+    if(this.length === 1){
+      let onlyVal = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return onlyVal;
+    }
+    
+    let currentVal = this.head;
+    let nextVal = this.head.next;
+    // loops through the whole list to find the last node in the list
+    while(nextVal.next){
+      currentVal = currentVal.next;
+      nextVal = nextVal.next;
+    }
+    // currentVal is the new tail of the linked list
+    // console.log(currentVal);
+    // nextVal is the node in the list that is getting removed
+    // console.log(nextVal);
+    // once the last node is found in the while loop; new tail is set to the second to last
+    // node; the last node's .next is null; and the length is decremented and
+    // return the value of the node that was removed
+
+    currentVal.next = null;
+    this.tail = currentVal;
+    this.length--;
+
+    return nextVal;
   }
 }
 
-// This code will work but getting the add to the list you have to keep doing .next at the end of first and wont have a head or tail
+// This code works but Nodes are used in the linked lists to hold the data; new Nodes hold the data and are able to hold a reference to the next node of data
 // var first = new Node("Hi")
 // first.next = new Node("there")
 // first.next.next = new Node("how")
@@ -38,7 +74,12 @@ class SinglyLinkedList{
 // console.log(first);
 
 var list = new SinglyLinkedList();
-list.push("HELLO");
-list.push("center");
-list.push("GOODBYE");
+list.push("1");
+list.push("2");
+list.push("3");
+list.push("4");
+list.push("5");
+list.push("6");
+console.log(list);
+list.pop();
 console.log(list);
