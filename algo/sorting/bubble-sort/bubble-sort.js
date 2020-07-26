2a0e569e079a82501201d9d74fd1799a361fd186
Zach Yingling <zpyingling9559@gmail.com>
2020-07-26 00:36:54 -0400

    finishing bubble sort with all of my notes and the most optimal version of the bubble sort

--- algo/sorting/bubble-sort/bubble-sort.js
+++ algo/sorting/bubble-sort/bubble-sort.js
@@ -1,6 +1,8 @@
 let numArr = [29, 10, 22, 1, 989, 21, 23, 2];
 
 // sorting algorithm that as it iterates, it 'bubbles' the largest values to the top/end of array
+// Big O(n^2) generally
+// But if the array is already nearly sorted it rounds down to Big O(n)
 
 function swap(arr, index1, index2){
   var temp = arr[index1];
@@ -20,6 +22,28 @@ function bubbleSort(arr){
   return arr;
 }
 
+// optimized bubble sort; outer loop starts at end of the arr and the inner function uses the outer function's variable i to not do a comparison to the 
+// last element in the array because you will be comparing a value and undefined OR a value that has already been compared and proven largest
+// noSwaps is also used to optimize the runtime; if a array is nearly sorted the algorithm will have to keep going through the array and prove everthing is
+// sorted. BUT if you use the noSwaps variable and nothing gets swapped it will remain true and break out of the for loops
+// Most useful with arrays that are already nearly sorted.
+function bubbleSortOptimized(arr){
+  let noSwaps;
+  for(let i = arr.length; i > 0; i--){
+    noSwaps = true;
+    for(let j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j + 1]){
+        swap(arr, j, j+1);
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
 console.log(numArr);
 let sortedArr = bubbleSort(numArr);
 console.log(sortedArr);
+let sortedOptimizedArr = bubbleSortOptimized(numArr);
+console.log(sortedOptimizedArr);
