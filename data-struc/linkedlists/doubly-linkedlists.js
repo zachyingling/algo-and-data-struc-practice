db14114fbb9be5993409c6da0300251db15a9f47
Zach Yingling <zpyingling9559@gmail.com>
2020-08-10 20:46:43 -0400

    finishing the push method in the dll

--- data-struc/linkedlists/doubly-linkedlists.js
+++ data-struc/linkedlists/doubly-linkedlists.js
@@ -15,4 +15,29 @@ class DoublyLinkedList{
     this.tail = null;
     this.length = 0;
   }
+  push(val){
+    // create new node
+    let newNode = new Node(val);
+    // if the list is empty; make head and tail this new node
+    if(this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    // else link the newNode to the tail both ways; being tail's next and newNode's prev
+    // then newNode is now the tail
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+let dll = new DoublyLinkedList;
+dll.push("first");
+dll.push("second");
+dll.push("third");
+console.log(dll);
