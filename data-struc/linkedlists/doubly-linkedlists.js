1763b3ddb5ee2055c81f6c65ce6e2b1a3a3cfd2a
Zach Yingling <zpyingling9559@gmail.com>
2020-08-10 21:22:32 -0400

    finishing pop method in dll

--- data-struc/linkedlists/doubly-linkedlists.js
+++ data-struc/linkedlists/doubly-linkedlists.js
@@ -34,10 +34,35 @@ class DoublyLinkedList{
     this.length++;
     return this;
   }
+  pop(){
+    let poppedValue;
+    // if the list is empty; else if length 1
+    if(!this.head){
+      return undefined;
+    } else if(this.length === 1){
+      poppedValue = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      // poppedValue is node getting popped
+      poppedValue = this.tail;
+      // new tail is the node previous to the one getting popped
+      this.tail = this.tail.prev;
+      // before this line of code \/; the .next is still the node gettting popped
+      this.tail.next = null;
+    }
+
+    // decrement length and return popped node
+    this.length--;
+    return poppedValue;
+  }
 }
 
 let dll = new DoublyLinkedList;
 dll.push("first");
 dll.push("second");
 dll.push("third");
+console.log(dll);
+dll.push("fourth");
+console.log(dll.pop());
 console.log(dll);
