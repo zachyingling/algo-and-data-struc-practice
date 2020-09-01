45e4f6efa5394f73d241b55120ac6fb699b6de81
Zach Yingling <zpyingling9559@gmail.com>
2020-09-01 15:06:57 -0400

    finishing notes for insert method

--- data-struc/Binary_Search_Trees/BST.js
+++ data-struc/Binary_Search_Trees/BST.js
@@ -11,25 +11,38 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value){
+        // create new node
         var newNode = new Node(value);
+        // if the tree is empty
         if(this.root === null){
             this.root = newNode;
             return this;
         }
+        // current gets updated as the while loop iterates
         var current = this.root;
         while(true){
+            // return undefined if the root and value is the same value
             if(value === current.value) return undefined;
+            // if value is less than current's value; goes to left side of tree
             if(value < current.value){
+                // if the left of the parent is null
+                // newNode is child of current
                 if(current.left === null){
                     current.left = newNode;
                     return this;
                 }
+                // go to the left of the tree
                 current = current.left;
             } else {
+                // else; go to right side of tree
+
+                // if the right of the parent is null
+                // newNode is child of current
                 if(current.right === null){
                     current.right = newNode;
                     return this;
-                } 
+                }
+                // go to the right of the tree
                 current = current.right;
             }
         }
@@ -67,7 +80,7 @@ class BinarySearchTree {
     }
 }
 
-
+// Insert Structure
 //      10
 //   5     13
 // 2  7  11  16
@@ -80,7 +93,3 @@ tree.insert(11)
 tree.insert(2)
 tree.insert(16)
 tree.insert(7)
-
-
-
-
