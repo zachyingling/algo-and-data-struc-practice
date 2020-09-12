65e3bab676f7716f7eb453a086a488ba472febf0
Zach Yingling <zpyingling9559@gmail.com>
2020-09-12 15:24:21 -0400

    finishing radix sort with comments in file

--- algo/sorting/radix-sort/radix-sort.js
+++ algo/sorting/radix-sort/radix-sort.js
@@ -1,10 +1,38 @@
 // Radix Sort
 
-// Big O
-// Time Complexity - 
-// Space Complexity - 
+// Big O [n being the nums.length and k being the number of digits in max digits]
+// Time Complexity - O(nk)
+// Space Complexity - O(n + k)
 
-function radixSort(){}
+// Sorts an array of numbers by the length of numbers
+// and numbers in the same length category iteratively.
+// 1(single digit), then 10(double digit), then 100, etc.
+// If there are more than value with identical digits then 
+// it would be compared by values
+
+function radixSort(nums){
+  // find the most amount of digits in the biggest value of the nums array
+  let maxDigitCount = mostDigits(nums);
+  
+  // iterate from 0 to the most amount of digits in the num array
+  for(let k = 0; k < maxDigitCount; k++){
+    // create one big array that is hold 10 subarrays
+    // which in return will create 10 separate empty (for now) buckets
+    let digitBuckets = Array.from({length: 10}, () => []);
+    // loop through each index of the nums array
+    for(let i = 0; i < nums.length; i++){
+      // go to the nums[i] index and get the digit at the k position of the value
+      // (nums[i] = 323; k = 1; what will be returned from getDigit is 2)
+      let digit = getDigit(nums[i],k);
+      // (nums[i] = 323; k = 1; what will be returned from getDigit is 2)
+      // push value 323 into bucket 2 out of the 10 buckets
+      digitBuckets[digit].push(nums[i]);
+    }
+    // its squeezes or concatenates all of the sub arrays into one big array (nums)
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
 
 // Assume base 10 numbers for the algorithm I wrote
 function getDigit(num, i){
@@ -13,7 +41,7 @@ function getDigit(num, i){
 
 // Pass in base 10 digit number
 function digitCount(num){
-  if (num === 0) return 1
+  if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
@@ -35,13 +63,15 @@ function mostDigits(nums){
 // Math.floor(73.23) = 73
 // 73 % 10 = 3
 // So given 7323, the number at the 2nd indeces is 3
-console.log(getDigit(7323, 2));
+console.log("getDigit function: " + getDigit(7323, 2));
 
 // if the parameter is 0 return 1
 // else return
 // base 10 logarithm of parameter
 // then remove the decimals and add one to the number being returned
-console.log(digitCount(642));
+console.log("digitCount function: " + digitCount(642));
 
 // returns the number of digits in the largest number in the list
-console.log(mostDigits([4,2,743,12,523461,7653121]));
+console.log("mostDigits function: " + mostDigits([4,2,743,12,523461,7653121]));
+
+console.log(radixSort([89,54,4,2,743,12,523461,7653121]));
