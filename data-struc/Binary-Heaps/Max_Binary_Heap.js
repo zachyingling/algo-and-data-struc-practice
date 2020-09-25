a8a5749e9f79656f977f4c558ddd5444c1ac80cf
Zach Yingling <zpyingling9559@gmail.com>
2020-09-24 23:14:42 -0400

    finishing extracting max method from max binary heap

--- data-struc/Binary-Heaps/Max_Binary_Heap.js
+++ data-struc/Binary-Heaps/Max_Binary_Heap.js
@@ -29,12 +29,79 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+  extractMax(){
+    // returnedValue is the max value of the binary heap
+    const returnedValue = this.values[0];
+    // the last value in the array
+    const endingValue = this.values.pop();
+    // conditional to make sure the array isnt empty
+    // if it would be empty it wouldn't run this code so the value 
+    // getting returned wouldn't be pushed back into the array
+    if(this.values.length > 0){
+      // set the first value to the value being popped off the end
+      this.values[0] = endingValue;
+      // sink the value of that got popped off down to the correct position
+      this.sinkDown();
+    }
+    return returnedValue;
+  }
+  // method that makes the first value be put into correct position
+  // if this method wouldn't be called in extractMax() it would leave
+  // the binary heap out of position
+  sinkDown(){
+    let parentIndex = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    // this while loop runs till swap variable never gets change and stays null
+    while(true){
+      // these calculations find the children index of the current parent
+      let leftChildIndex = 2 * parentIndex + 1;
+      let rightChildIndex = 2 * parentIndex + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      // conditonal that makes sure the children index isnt out of bounds of the array
+      if(leftChildIndex < length){
+        leftChild = this.values[leftChildIndex];
+        // if the value of the left child is greater than the parent value
+        // give swap the value of the left child index so it can be swaped at the end of the while loop
+        if(leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      // conditonal that makes sure the children index isnt out of bounds of the array
+      if(rightChildIndex < length){
+        rightChild = this.values[rightChildIndex];
+        // two conditionals in one
+        // checks if right child is greater than the element and if swap hasn't been touched
+        // OR
+        // if swap has been changed checks to see if the right child is greater than the left to see which child to follow
+        if(
+          (swap === null && rightChild > element) || 
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      // breaks out of the while loop if the children are less than the parent
+      // so the current value is in the proper position and shouldnt be moved
+      if(swap === null) break;
+      // if the while loop continues swap values of the current value you are
+      // sinking down and whichever child is a greater value (left or right)
+      this.values[parentIndex] = this.values[swap];
+      this.values[swap] = element;
+      parentIndex = swap;
+    }
+  }
 }
 
 // [41,39,33,18,27,12]
 //  0, 1, 2, 3, 4, 5
 
 let heap = new MaxBinaryHeap();
+// Insert value into binary heap
 heap.insert(41);
 heap.insert(39);
 heap.insert(33);
@@ -42,4 +109,8 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+console.log(heap);
+// extract max value of binary heap
+// since its a max binary heap it will always be the root of the heap
+console.log(heap.extractMax());
 console.log(heap);
