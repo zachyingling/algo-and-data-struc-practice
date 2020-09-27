fc2755ffbb24f0a9cadcdd5896c7199e2aafa193
Zach Yingling <zpyingling9559@gmail.com>
2020-09-27 18:44:16 -0400

    adding structure to priority queue and notes on priority queue

---
+++ data-struc/Binary-Heaps/Priority_Queue.js
@@ -0,0 +1,13 @@
+// What is a Priority Queue?
+// A data structure where each element has a priority.
+// Elements with higher priorities are served before elements with lower
+// priorities.
+
+// Priority Queue is just an abstract concept. It can be done in many ways.
+// Lots of the time priority queues are done using Binary Heaps due to speed and simplicity
+
+class PriorityQueue {
+  constructor(){
+    this.values = [];
+  }
+}
