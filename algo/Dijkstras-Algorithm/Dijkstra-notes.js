a2c0a42035e77e6f38a2766b9f80cfae18aa7e04
Zach Yingling <zpyingling9559@gmail.com>
2020-10-06 20:17:07 -0400

    finishing notes on dijkstra algorithm

---
+++ algo/Dijkstras-Algorithm/Dijkstra-notes.js
@@ -0,0 +1,18 @@
+// Dijkstra's Algorithm
+
+// Algorithm for finding the shortest paths between nodes in a graph
+
+// One of the most famous and widely used algorithms around
+// Finds the shortest path between two vertices on a graph
+// "What's the fastest way to get from point A to point B"
+
+// Edsger Disjkstra was a Dutch programmer, physicist, essayist, and all around smarty-pants
+// He helped to advance the field of computer science from an "art" to an academic discipline
+// Many of his discoveries and algorithms are still commonly used to this day
+
+// Useful for
+// = GPS - finding fastest route
+// = Network Routing - finds open shortest path for data
+// = Biology - used to model the spread of viruses among humans
+// = Airline tickets - finding cheapest route to your destination
+// ...many other uses
